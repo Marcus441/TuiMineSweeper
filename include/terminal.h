05c3fd25d3ab9006b1f81aade1234ff03e5672ca
Marcus441 <psminchia4153@hotmail.com>
2026-02-18 13:09:17 +1000

    refactor(terminal): terminal handler now created
    - Added a singleton class to handle terminal configuration

---
+++ include/terminal.h
@@ -0,0 +1,17 @@
+#include <sys/ioctl.h>
+#include <termios.h>
+
+class TerminalHandler {
+public:
+  TerminalHandler();
+  ~TerminalHandler();
+
+  // Disable copying
+  TerminalHandler(const TerminalHandler &) = delete;
+  TerminalHandler &operator=(const TerminalHandler &) = delete;
+
+private:
+  struct termios old_termios, new_termios;
+  void enableRawMode();
+  void resetTerminal();
+};
---
+++ src/terminal.cpp
@@ -0,0 +1,27 @@
+#include "terminal.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <ostream>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+void TerminalHandler::enableRawMode() {
+  tcgetattr(STDIN_FILENO, &old_termios);
+  new_termios = old_termios;               // copy the termios state
+  new_termios.c_lflag &= ~(ICANON | ECHO); // put terminal into raw mode by
+                                           // disabling canonical input and echo
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &new_termios);
+}
+
+void TerminalHandler::resetTerminal() {
+  std::cout << "\e[m" << "\e[?25h" << std::endl;
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &old_termios);
+}
+
+TerminalHandler::~TerminalHandler() { this->resetTerminal(); }
+
+TerminalHandler::TerminalHandler() {
+  enableRawMode();
+  std::cout << "\e[?25l" << std::flush; // hide cursor
+}
